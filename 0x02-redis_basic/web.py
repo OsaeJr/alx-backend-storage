7173fd508b4b14d38396f578a64c59fffecb7474
OsaeJr <samueladjar11@.com>
2024-10-23 09:17:32 -0700

    Add files

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,55 +1,34 @@
 #!/usr/bin/env python3
-"""Module for implementing an expiring web cache and tracker
-"""
 import requests
 import time
 from functools import wraps
+from typing import Dict
 
-CACHE_EXPIRATION_TIME = 10  # seconds
-CACHE = {}
+cache: Dict[str, str] = {}
 
-
-def cache(fn):
-    """_summary_
-
-    Args:
-        fn (function): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    @wraps(fn)
-    def wrapped(*args, **kwargs):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
-        url = args[0]
-        if url in CACHE and CACHE[url]["timestamp"] + CACHE_EXPIRATION_TIME > \
-                time.time():
-            CACHE[url]["count"] += 1
-            return CACHE[url]["content"]
-        else:
-            content = fn(*args, **kwargs)
-            CACHE[url] = {"content": content,
-                          "timestamp": time.time(), "count": 1}
-            return content
-    return wrapped
-
-
-@cache
 def get_page(url: str) -> str:
-    """_summary_
+    if url in cache:
+        print(f"Retrieving from cache: {url}")
+        return cache[url]
+    else:
+        print(f"Retrieving from web: {url}")
+        response = requests.get(url)
+        result = response.text
+        cache[url] = result
+        return result
 
-    Args:
-        url (str): _description_
-
-    Returns:
-        str: _description_
-    """
-    global count
-    # increment count
-    count += 1
-    response = requests.get(url)
-    return response.content.decode('utf-8')
+def cache_with_expiration(expiration: int):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            url = args[0]
+            key = f"count:{url}"
+            if key in cache:
+                count, timestamp = cache[key]
+                if time.time() - timestamp > expiration:
+                    result = func(*args, **kwargs)
+                    cache[key] = (count+1, time.time())
+                    return result
+                else:
+                    cache[key] = (count+1, timestamp)
+                    return
